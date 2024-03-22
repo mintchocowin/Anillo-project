9257800eaee5019e08b263150904ec1613cfddfe
mintchocowin <sliuy59@gmail.com>
2024-03-22 17:27:07 +0900

    240322mjmj

--- Index/script.js
+++ Index/script.js
@@ -1,11 +1,8 @@
-//section05
+//section5 parallax
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const parallax = document.querySelector(".parallax-image");
-//   const speed = 0.5;
+const parallax = document.querySelector(".index-section05");
 
-//   window.addEventListener("scroll", function () {
-//     const yOffset = window.pageYOffset;
-//     parallax.style.transform = "translateY(" + yOffset * speed + "px)";
-//   });
-// });
+window.addEventListener("scroll", function () {
+  const offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+});
