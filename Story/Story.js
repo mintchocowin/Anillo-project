429284c7211c2d9d65156b7518e452ee3a33867b
mintchocowin <sliuy59@gmail.com>
2024-03-25 14:04:42 +0900

    240325.mj

--- Story/Story.js
+++ Story/Story.js
@@ -1,18 +1,18 @@
 //story1
 
-const section1Image = document.querySelector(".section1-main");
-const innerHeight = window.innerHeight;
-window.addEventListener("scroll", function () {
-  const scrollY = window.scrollY;
-  const maxScroll = innerHeight; // 이미지를 최대로 확대하길 원하는 스크롤 위치
-  const minScale = 1; // 이미지의 최소 스케일
-  const maxScale = 1.5; // 이미지의 최대 스케일
+// const section1Image = document.querySelector(".section1-main");
+// const innerHeight = window.innerHeight;
+// window.addEventListener("scroll", function () {
+//   const scrollY = window.scrollY;
+//   const maxScroll = innerHeight; // 이미지를 최대로 확대하길 원하는 스크롤 위치
+//   const minScale = 1; // 이미지의 최소 스케일
+//   const maxScale = 1.5; // 이미지의 최대 스케일
 
-  let scale = minScale + (maxScale - minScale) * (scrollY / maxScroll);
-  scale = Math.min(maxScale, Math.max(minScale, scale)); // scale 값을 최소 및 최대 스케일 값으로 제한
+//   let scale = minScale + (maxScale - minScale) * (scrollY / maxScroll);
+//   scale = Math.min(maxScale, Math.max(minScale, scale)); // scale 값을 최소 및 최대 스케일 값으로 제한
 
-  section1Image.style.transform = `scale(${scale})`;
-});
+//   section1Image.style.transform = `scale(${scale})`;
+// });
 //story3
 
 const scrollEvent3 = () => {
